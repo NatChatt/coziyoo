44ed33c7c2743f18cea8b48649d35308ed5c0c29
ismetkarakus <ismetkarakus1@hotmail.com>
2026-05-21 17:30:38 +0100

    Bypass CSRF origin check for admin login

--- apps/django/coziyoo/urls.py
+++ apps/django/coziyoo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.decorators.csrf import csrf_exempt
 from apps.foods.views import FoodImageView, FavoriteListView, FavoriteToggleView
 from apps.authentication.admin_home_hero import home_hero_view, hero_image_proxy_view
 from coziyoo.admin_search import admin_global_search
@@ -15,6 +16,7 @@ urlpatterns = [
     path("admin/home-hero/image/", admin.site.admin_view(hero_image_proxy_view), name="admin_home_hero_image"),
     path("admin/global-search/", admin_global_search, name="admin_global_search"),
     path("admin/dashboard/data/", admin.site.admin_view(dashboard_data), name="admin_dashboard_data"),
+    path("admin/login/", csrf_exempt(admin.site.login), name="admin_login"),
     path("admin/", admin.site.urls),
     path("food-images/<uuid:food_id>", FoodImageView.as_view(), name="food-public-image"),
     path("i18n/", include("django.conf.urls.i18n")),
